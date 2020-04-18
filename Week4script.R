169f913b8fadd25e9e43b3350303741d601f4575
JonathanYde <jonathan.yde@biomed.au.dk>
2020-04-18 16:22:31 +0200

    finishes the last question

--- Week4script.R
+++ Week4script.R
@@ -49,7 +49,7 @@ Baltimore.city2 <- NEI[which(NEI$fips == 24510), ]
 sum.baltimore <- with(Baltimore.city2, tapply(Emissions, list(type,year), sum, na.rm=TRUE))
 
 #Prepares to create a plot with the different emission types in Baltimore City
-plot(x = c(1999,2002,2005,2008), y = sum.baltimore[2,], col=1, pch=20, type="n", ylim = c(0,2200), main = "Emissions in Baltimore City", ylab = "PM2.5 in tonnes")
+plot(x = c(1999,2002,2005,2008), y = sum.baltimore[2,], col=1, pch=20, type="n", ylim = c(0,2200), main = "Emissions in Baltimore City", xlab = "Year", ylab = "PM2.5 in tonnes")
 
 #Adds points and lines for "Non-road" emissions
 points(x = c(1999,2002,2005,2008), y = sum.baltimore[1,], col=1, pch=20)
@@ -100,11 +100,57 @@ barplot(total.emissions2, main = "PM2.5 emissions from coal combustion", ylab = 
 
 ##### How have emissions from motor vehicle sources changed from 1999–2008 in Baltimore City? #####
 
+#Stores a dataframe with all entries with "vehicle" in the short name
+vehicles <- SCC[which(grepl("Vehicle",x = SCC$Short.Name)),]
+
+#Stores a dataframe of PM2.5 emissions where SCC matches the SCCs from "vehicles"
+vehicle.emissions <- NEI[which(NEI$SCC %in% vehicles$SCC),]
+
+#Stores a dataframe of PM2.5 emissions from vehicles in Baltimore City
+Baltimore.city.vehicles <- vehicle.emissions[which(vehicle.emissions$fips == 24510), ]
+
+#Calculates the sums of emissions for each year from vehicles in Baltimore City
+sum.baltimore.vehicles <- with(Baltimore.city.vehicles, tapply(Emissions, as.factor(year), sum, na.rm=TRUE))
+
+#Creates a plot with lines for vehicle emissions in Baltimore City
+plot(x = c(1999,2002,2005,2008), y = sum.baltimore.vehicles, col=1, pch=20, main = "Vehicle missions in Baltimore City", xlab= "Year", ylab = "PM2.5 in tonnes")
+lines(x = c(1999,2002,2005,2008), y = sum.baltimore.vehicles, col=1)
 
 ##### Compare emissions from motor vehicle sources in Baltimore City with emissions #####
-# from motor vehicle sources in Los Angeles County, California (fips == "06037"\color{red}{\verb|fips == "06037"|}fips=="06037").
+# from motor vehicle sources in Los Angeles County, California (fips == "06037").
 # Which city has seen greater changes over time in motor vehicle emissions?
 
+#Stores a dataframe of PM2.5 emissions from vehicles in Los Angeles County
+LA.vehicles <- vehicle.emissions[which(vehicle.emissions$fips == "06037"), ]
+
+#Calculates the sums of emissions for each year from vehicles in Los Angeles County
+sum.LA.vehicles <- with(LA.vehicles, tapply(Emissions, as.factor(year), sum, na.rm=TRUE))
+
+#Creates a plot with lines for vehicle emissions in Los Angeles County
+plot(x = c(1999,2002,2005,2008), y = sum.LA.vehicles, col=1, pch=20, main = "Vehicle missions in LA and Baltimore City", xlab= "Year", ylab = "PM2.5 in tonnes", ylim=c(0,1500))
+lines(x = c(1999,2002,2005,2008), y = sum.LA.vehicles, col=1)
+
+#Adds points and lines for vehicle emissions in Baltimore City
+points(x = c(1999,2002,2005,2008), y = sum.baltimore.vehicles, col=2, pch=20)
+lines(x = c(1999,2002,2005,2008), y = sum.baltimore.vehicles, col=2)
+
+#Adds a legend
+legend("left", legend=c("LA","Baltimore"),
+       col=c("black", "red"), lty=1, cex=0.8,
+       box.lty=1)
+
+#Creates a plot with lines for vehicle emissions in Los Angeles County
+plot(x = c(1999,2002,2005,2008), y = log10(sum.LA.vehicles), col=1, pch=20, main = "Vehicle missions in LA and Baltimore City", xlab= "Year", ylab = "log10 (PM2.5 in tonnes)", ylim=c(1.3,3.2))
+lines(x = c(1999,2002,2005,2008), y = log10(sum.LA.vehicles), col=1)
+
+#Adds points and lines for vehicle emissions in Baltimore City
+points(x = c(1999,2002,2005,2008), y = log10(sum.baltimore.vehicles), col=2, pch=20)
+lines(x = c(1999,2002,2005,2008), y = log10(sum.baltimore.vehicles), col=2)
+
+#Adds a legend
+legend("right", legend=c("LA","Baltimore"),
+       col=c("black", "red"), lty=1, cex=0.8,
+       box.lty=1)
 
 ##### For each plot you should #####
 
