d2104639e8aee7edf60da3a8a26008420aba2cbc
JonathanYde <jonathan.yde@biomed.au.dk>
2020-04-19 10:06:55 +0200

    last plot

--- plot6.R
+++ plot6.R
@@ -10,6 +10,18 @@ SCC <- readRDS("data/Source_Classification_Code.rds")
 # from motor vehicle sources in Los Angeles County, California (fips == "06037").
 # Which city has seen greater changes over time in motor vehicle emissions?
 
+#Stores a dataframe with all entries with "vehicle" in the short name
+vehicles <- SCC[which(grepl("Vehicle",x = SCC$Short.Name)),]
+
+#Stores a dataframe of PM2.5 emissions where SCC matches the SCCs from "vehicles"
+vehicle.emissions <- NEI[which(NEI$SCC %in% vehicles$SCC),]
+
+#Stores a dataframe of PM2.5 emissions from vehicles in Baltimore City
+Baltimore.city.vehicles <- vehicle.emissions[which(vehicle.emissions$fips == 24510), ]
+
+#Calculates the sums of emissions for each year from vehicles in Baltimore City
+sum.baltimore.vehicles <- with(Baltimore.city.vehicles, tapply(Emissions, as.factor(year), sum, na.rm=TRUE))
+
 #Stores a dataframe of PM2.5 emissions from vehicles in Los Angeles County
 LA.vehicles <- vehicle.emissions[which(vehicle.emissions$fips == "06037"), ]
 
@@ -17,6 +29,7 @@ LA.vehicles <- vehicle.emissions[which(vehicle.emissions$fips == "06037"), ]
 sum.LA.vehicles <- with(LA.vehicles, tapply(Emissions, as.factor(year), sum, na.rm=TRUE))
 
 #Creates a plot with lines for vehicle emissions in Los Angeles County
+png(filename = "plot6.png")
 plot(x = c(1999,2002,2005,2008), y = sum.LA.vehicles, col=1, pch=20, main = "Vehicle missions in LA and Baltimore City", xlab= "Year", ylab = "PM2.5 in tonnes", ylim=c(0,1500))
 lines(x = c(1999,2002,2005,2008), y = sum.LA.vehicles, col=1)
 
@@ -28,16 +41,4 @@ lines(x = c(1999,2002,2005,2008), y = sum.baltimore.vehicles, col=2)
 legend("left", legend=c("LA","Baltimore"),
        col=c("black", "red"), lty=1, cex=0.8,
        box.lty=1)
-
-#Creates a plot with lines for vehicle emissions in Los Angeles County
-plot(x = c(1999,2002,2005,2008), y = log10(sum.LA.vehicles), col=1, pch=20, main = "Vehicle missions in LA and Baltimore City", xlab= "Year", ylab = "log10 (PM2.5 in tonnes)", ylim=c(1.3,3.2))
-lines(x = c(1999,2002,2005,2008), y = log10(sum.LA.vehicles), col=1)
-
-#Adds points and lines for vehicle emissions in Baltimore City
-points(x = c(1999,2002,2005,2008), y = log10(sum.baltimore.vehicles), col=2, pch=20)
-lines(x = c(1999,2002,2005,2008), y = log10(sum.baltimore.vehicles), col=2)
-
-#Adds a legend
-legend("right", legend=c("LA","Baltimore"),
-       col=c("black", "red"), lty=1, cex=0.8,
-       box.lty=1)
+dev.off()
